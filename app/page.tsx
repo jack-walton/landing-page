c7c7f3daf91177ba1011651220c4efd60e44a105
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-08-29 18:14:33 -0500

    Update paragraph margin

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,10 @@ export default function Page() {
     <section className="max-w-xl mx-auto">
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Jack Spencer Walton</h1> 
        
-   <p>
+   <p className="mb-8">
     I'm a writer, researcher, and content designer. For the last four years, I've worked as a technical writer for the UX Design organization at Oracle, where I work on user assistance documentation and write user interface copy for healthcare products. I graduated <em>magna cum laude</em> from the University of Missouri - Kansas City with a BA in English and a minor in medieval and early modern studies. While in school, I worked as a research assistant for CODICES Digital Humanities Lab.
   </p>
-
-<div data-theme="system" className="tweet-container">
+<div data-theme="system">
       <Tweet
         id="1858197154772250842"
       />
