f7454a43a10eda6c686f98c2cc395ed696a04ba7
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-08-29 16:23:36 -0500

    Develop "about" copy

--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,18 @@ export const metadata = {
 export default function Page() {
   return (
     <section className="max-w-xl mx-auto">
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Jack Spencer Walton</h1>
-
-      <p className="mb-8">
-        I'm a writer, researcher, and content designer currently based out of Kansas City.
-      </p>
-
-            <Image
-        src="/headshot.png"
-        alt="Jack Spencer Walton"
-        width={1000}
-        height={200}
-        className="rounded-full mb-4"
-      />
-
-
-
-
-    <div data-theme="system" style={{ maxWidth: '500px', margin: '0 auto' }}>
-      <Tweet id="1858197154772250842" />
-    </div>
-
-
-
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Jack Spencer Walton</h1>   
+   <p className="flex-1 mb-8">
+    I'm a writer, researcher, and content designer. For the last four years, I've worked as a technical writer for the UX Design organization at Oracle, where I work on user assistance documentation and write user interface copy for healthcare products. I graduated <em>magna cum laude</em> from the University of Missouri - Kansas City with a BA in English and a minor in medieval and early modern studies. While in school, I worked as a research assistant for CODICES Digital Humanities Lab. I am an Eagle Scout.
+  </p>
+  
+  <Image
+    src="/headshot.png"
+    alt="Jack Spencer Walton"
+    width={1000}
+    height={200}
+    className="rounded-full"
+  />
     </section>
   );
 }
