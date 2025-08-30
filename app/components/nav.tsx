d9cfb45e2d584ff2c7b1134a1b521123a621f05a
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-08-29 20:04:13 -0500

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,22 +2,19 @@ import Link from 'next/link'
 
 const navItems = {
   '/': {
-    name: 'home',
+    name: 'about',
   },
   '/projects': {
     name: 'projects',
-  },
-  '/blog': {
-    name: 'blog',
   },
 }
 
 export function Navbar() {
   return (
-    <aside className="mb-4 mt-4 tracking-tighter text-center border-b-1 border-t-1 border-gray-300 py-4"> {/* Removed -ml-[8px] to avoid offset */}
-      <div className="lg:sticky lg:top-20">
+    <aside className="mb-4 mt-4 tracking-tighter border-b-1 border-t-1 border-gray-300 py-4"> {/* Removed -ml-[8px] to avoid offset */}
+      
         <nav
-          className="flex flex-row items-center justify-center relative px-0 pb-0 fade md:overflow-auto md:relative"
+          className="flex flex-row relative px-0 pb-0 fade md:overflow-auto md:relative"
           id="nav"
         >
           <div className="flex flex-row space-x-4"> {/* Simplified, removed pr-10 */}
@@ -34,7 +31,7 @@ export function Navbar() {
             })}
           </div>
         </nav>
-      </div>
+      
     </aside>
   )
 }
